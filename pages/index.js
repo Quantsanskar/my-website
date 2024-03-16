be78fea3cebd448cf2b3891fe34d2b769f669ae6
Sanskar Bhardwaj <sanskarsrdav@gmail.com>
2024-03-17 03:06:56 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,32 @@
 // pages/index.js
 import React from 'react';
+import ErrorBoundary from '../components/ErrorBoundary';
 import Navbar from '../components/Navbar';
 import HomePage from '../components/HomePage';
 import LocationSection from '../components/LocationSection';
 import MentorsSection from '../components/MentorsSection';
 import AdmissionButton from '../components/AdmissionButton';
-import ThemeToggle from '../components/ThemeToggle';
 
 const MainPage = () => {
     return (
-        <div>
-            <Navbar />
-            <ThemeToggle />
-            <div id="about-us">
-                <HomePage />
+        <ErrorBoundary>
+            <div>
+                <Navbar/>
+               
+                <div id="about-us">
+                    <HomePage />
+                </div>
+                <div id="location">
+                    <LocationSection />
+                </div>
+                <div id="mentors">
+                    <MentorsSection />
+                </div>
+                <div id="admission-button">
+                    <AdmissionButton />
+                </div>
             </div>
-            <div id="location">
-                <LocationSection />
-            </div>
-            <div id="mentors">
-                <MentorsSection />
-            </div>
-            <div id="admission-button">
-                <AdmissionButton />
-            </div>
-        </div>
+        </ErrorBoundary>
     );
 };
 
