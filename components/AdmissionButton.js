3a534dc06cc0653f56b125a7a5c5f751c8cf3067
Sanskar Bhardwaj <sanskarsrdav@gmail.com>
2024-03-17 17:39:18 +0530

    Update AdmissionButton.js

--- components/AdmissionButton.js
+++ components/AdmissionButton.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AdmissionForm from './AdmissionForm';
 
 const AdmissionButton = () => {
+    const [isFormOpen, setIsFormOpen] = useState(false);
+
+    const openForm = () => {
+        setIsFormOpen(true);
+     };
+
+    const closeForm = () => {
+        setIsFormOpen(false);
+        document.body.classList.remove('decreased-light');
+    };
+
     return (
-        <div className="admission-button">
-            <button>Join and Apply for Admission</button>
+        <div className="admissionButtonWrapper">
+            <div className="admissionButtonContainer">
+                <button onClick={openForm}>Contact Us</button>
+            </div>
+            {isFormOpen && <AdmissionForm onClose={closeForm} />} {/* Pass onClose as a prop */}
         </div>
     );
 };
