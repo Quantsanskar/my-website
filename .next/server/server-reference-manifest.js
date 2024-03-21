479a06a1e2b2bd753a3ebcaeb2e5342fa4c5317c
komal Satyawali <komal01827@gmail.com>
2024-03-21 11:47:21 +0530

    bg changed to red

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< Updated upstream
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qxxBev5Dzxo6y7eg6sBHAzF1zBqIlrUI4pd4ZxDMaso=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CvFgaiUq+bj3klwFDxQjnRPdnr+JLdqtOtaJ1jtEsHA=\"\n}"
->>>>>>> Stashed changes
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"VVBoRusGvR66Wppd2Q8V9FDYl4kDfq20+Ru8iZLweTU=\"}"
