321fcd419307add4ac9d8add199f14815a1da908
Sanskar Bhardwaj <sanskarsrdav@gmail.com>
2024-03-21 23:52:27 +0530

    Error is resolved now

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"VVBoRusGvR66Wppd2Q8V9FDYl4kDfq20+Ru8iZLweTU=\"}"
+=======
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"rigmj4Vib9S79hWHFgVDNJhHAGnodantGzrO2TCy4cY=\"\n}"
+>>>>>>> Stashed changes
