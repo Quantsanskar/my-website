548a6f5fcff1d47f299f2e50ca725d409c69ae81
Revant <86226975+revant7@users.noreply.github.com>
2024-04-03 20:46:33 +0530

    Pulled Changes

--- backend/institute/views.py
+++ backend/institute/views.py
@@ -22,8 +22,8 @@ class UserListAPIView(generics.ListAPIView):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+        return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # class UserDetailAPIView(generics.GenericAPIView):
--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-export const plugins = {
-  tailwindcss: {},
+module.exports = {
+  // tailwindcss: {},
   autoprefixer: {},
 };
