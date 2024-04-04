880debf3b7b86d10eea3414527c913fedc8c534b
Revant <86226975+revant7@users.noreply.github.com>
2024-04-04 19:57:33 +0530

    Added Twilio

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
-  // tailwindcss: {},
-  autoprefixer: {},
-=======
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
->>>>>>> 2af2cf245f77cab40be02827ba964c63dcff4fc4
 };
