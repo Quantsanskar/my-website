16fee8b975c904f9e9fc83875b2ee4147b279cd7
Sanskar Bhardwaj <sanskarsrdav@gmail.com>
2024-04-06 17:40:43 +0530

    made small changes in signin form

--- components/SignInForm.js
+++ components/SignInForm.js
@@ -37,7 +37,7 @@ const SignInForm = ({ isSignInOpen, closeSignIn }) => {
       console.error('Error:', error);
       setError('An unexpected error occurred');
     }
-    closeSignIn();
+    // closeSignIn();
   };
 
   return (
